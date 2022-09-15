13af06abd7ed04a98546732280a040237d833e8f
mscode98 <workmarco98@gmail.com>
2022-09-15 23:33:18 +0100

    IO: Dead code. Using asm volatile instead of calling extern

--- boot.s
+++ boot.s
@@ -8,25 +8,11 @@ section .text
 
 global _start
 global keyboard_handler
-global read_port
-global write_port
 global load_idt
 global gdt_flush
 
 extern kernel_main 		;this is defined in the c file
 extern keyboard_handler_main
-
-read_port:
-	mov edx, [esp + 4]
-			;al is the lower 8 bits of eax
-	in al, dx	;dx is the lower 16 bits of edx
-	ret
-
-write_port:
-	mov   edx, [esp + 4]    
-	mov   al, [esp + 4 + 4]  
-	out   dx, al  
-	ret
 
 gdt_flush:
 	mov edx, [esp + 4]
