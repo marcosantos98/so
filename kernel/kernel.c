85fc1724b3e8fdbeea9e075c65a7ae0b72ae5458
Marco Santos <101365429+mscode98@users.noreply.github.com>
2022-09-17 12:36:57 +0100

    Misc: Remove compiler checks. We now have a toolchain script to avoid it

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,16 +7,6 @@
 #include "video.h"
 #include "keyboard.h"
 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
-#if defined(__linux__)
-// "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
-
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-// "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
-
 void kernel_main(void)
 {
 	video_main();
